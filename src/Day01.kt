d65ec27d8315ea7165a45367afec06cabe33616a
Stephen Vinouze <s.vinouze@gmail.com>
2022-12-01 10:33:20 +0200

    Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,23 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun caloriesPerEfl(input: List<String>): List<Int> {
+        var caloriesPerElf = 0
+        val elfCalories = mutableListOf<Int>()
+        input.forEachIndexed { index, calories ->
+            if (calories.isBlank() || index == input.lastIndex) {
+                elfCalories.add(caloriesPerElf)
+                caloriesPerElf = 0
+            } else {
+                caloriesPerElf += calories.toInt()
+            }
+        }
+        return elfCalories
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+        caloriesPerEfl(input).max()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(input: List<String>): Int =
+        caloriesPerEfl(input).sortedDescending().take(3).sum()
 
     val input = readInput("Day01")
     println(part1(input))
